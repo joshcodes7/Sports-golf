bad4bff1f6ff994fccadfd6789f81acf3353e9a3
Joshna <joshna.salu@allegiantair.com>
2026-06-05 16:45:35 +0530

    Initial commit.

---
+++ src/test/java/com/golf/Sports/SportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golf.Sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
